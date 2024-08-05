fc7d1c47da5b2b8d867c132919600a69d93e20cc
Phuc <thpgalaxy2003@gmail.com>
2024-08-06 02:17:32 +0700

    init project

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -3,6 +3,9 @@ package hello;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		int a;
+		a = 100 - 27 + 8 * 2 / 10;
+		System.out.println("Ket qua: " + a);
 
 	}
 }
